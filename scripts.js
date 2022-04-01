028c4db3b865b7d8f4216c4fc829fffabeb9f0bf
unknown <ekeinchiaemmanuela@gmail.com>
2022-04-01 10:08:02 -0700

    linking JavaScript

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
-const main = document.querySelector('.main')
+'use strict'
 
-main.addEventListener('click', () =>{
+const main = document.querySelector(".main");
+const mainSection = document.querySelector(".fas fa-comment");
 
-})
+fasFaComment.addEventListener("click", () => {
+  const msg = document.querySelector(".msg").classList.add(".main");
+  main.classList.add(".hide");
+  console.log(msg);
+});
+console.log(msg)
